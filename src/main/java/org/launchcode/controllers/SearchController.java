c283856e84bccdb0d0640e21d6619f102011d156
Sarah Goldacker <crossbreedsarah@gmail.com>
2019-04-03 03:21:13 -0500

    think i finished mvc2

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -23,5 +23,16 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
-
+    @RequestMapping(value = "results")
+    public String search (Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<HashMap<String, String>> jobs;
+        if (searchType.equals("all") || searchTerm.equals("")){
+            jobs = JobData.findByValue(searchTerm);
+        }else{
+            jobs = JobData.findByValue(searchTerm);
+        }
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
 }
